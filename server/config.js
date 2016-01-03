b5bb8c05922846896d898e55c234026a4bc08fae
Maximilian Fellner <max.fellner@gmail.com>
2016-01-03 13:02:07 +0100

    feat: use postgres schema and yaml configuration

--- server/config.js
+++ server/config.js
@@ -1,6 +1,8 @@
 import nconf from 'nconf'
+import yaml from 'js-yaml'
+import fs from 'fs'
 
 export default nconf
   .argv()
   .env()
-  .defaults(require('../config.json'))
+  .defaults(yaml.safeLoad(fs.readFileSync('./config.yml', 'utf8')))
--- server/model.js
+++ server/model.js
@@ -82,11 +82,20 @@ export const Session = sequelize.define('session', {
   }
 })
 
-export function syncModel() {
+export async function syncModel() {
+  const schema = config.get('DB_SCHEMA')
+  const schemas = await sequelize.showAllSchemas().
+  catch(err => log('error fetching schemas', err))
+
+  if (schemas.indexOf(schema) === -1) {
+    await sequelize.createSchema(schema).
+    catch(err => log('error creating schema', err))
+  }
+
   return Promise.all([
-    User.sync(),
-    Repository.sync(),
-    Session.sync()
+    User.schema(schema).sync(),
+    Repository.schema(schema).sync(),
+    Session.schema(schema).sync()
   ]).
   then(models => log('synced models %o', models)).
   catch(err => log('error syncing model', err))
