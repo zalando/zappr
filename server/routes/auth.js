9176e87c886295a8f374c985f9eedf206d167f08
Nikolaus Piccolotto <prayerslayer@gmail.com>
2016-02-29 13:44:32 +0100

    Short-circuit authentication if TOKEN is in env
    for local testing

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -37,6 +37,12 @@ export function logout(router) {
  * Middleware to require authentication.
  */
 export function requireAuth(ctx, next) {
+  if (process.env.GITHUB_ACCESS_TOKEN) {
+    ctx.req.user = {
+      accessToken: process.env.GITHUB_ACCESS_TOKEN
+    }
+    return next()
+  }
   if (ctx.isAuthenticated()) {
     return next()
   } else {
