7fa35c433716358d51b8f78095ee49ad0ac54ca0
Nikolaus Piccolotto <prayerslayer@gmail.com>
2016-03-04 10:54:39 +0100

    #35 infotext

--- client/components/RepositoryCheck.jsx
+++ client/components/RepositoryCheck.jsx
@@ -4,7 +4,14 @@ import { Panel, Well } from 'react-bootstrap'
 import Toggle from './Toggle.jsx'
 
 const INFO_TEXT = {
-  approval: 'TODO approval infotext'
+  approval: <p>Once you enabled the Approval check, ZAPPR will set a
+               commit status to every PR in your repository. It will look
+               comments that match a regex (default: <code>/^:\+1:$/</code>) and count
+               those as approval to merge. If your PR has enough approvals 
+               (default: <code>2</code>) the status will be set to successful.
+               By default ZAPPR will count comments from anyone, but you can
+               configure this in your <a href='https://github.com/zalando/zappr/blob/master/example-zappr.yml'>
+               <code>zappr.yml</code></a> file.</p>
 }
 
 export default class RepositoryCheck extends Component {
