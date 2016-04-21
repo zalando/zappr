6febe6be9f9aa6b378408528e2cf5ed4cacd3869
Nikolaus Piccolotto <prayerslayer@gmail.com>
2016-04-21 16:40:07 +0200

    #106 move docs into link after check name

--- client/components/RepositoryCheck.jsx
+++ client/components/RepositoryCheck.jsx
@@ -4,15 +4,15 @@ import { Alert, Panel, Well } from 'react-bootstrap'
 import Toggle from './Toggle.jsx'
 
 const INFO_TEXT = {
-  approval: <p>The approval feature blocks a pull request (if you enabled <a href='https://github.com/blog/2051-protected-branches-and-required-status-checks'>protected branches</a>)
-               until it has the required amount of approvals. For configuration options please consult <a href='https://zappr.readthedocs.org/en/latest/setup/#approvals'>our docs</a>.
+  approval: <p>The approval feature (<a href='https://zappr.readthedocs.org/en/latest/setup/#approvals'>docs</a>) blocks a pull request (if you
+               enabled <a href='https://github.com/blog/2051-protected-branches-and-required-status-checks'>protected branches</a>)
+               until it has the required amount of approvals.
             </p>,
-  autobranch: <p>When enabling automatic branch creation, ZAPPR will automatically
-                 create a branch in your repository for every opened issue. For configuration options please consult
-                 <a href='https://zappr.readthedocs.org/en/latest/setup/#autobranch'>our docs</a>
+  autobranch: <p>When enabling automatic branch creation (<a href='https://zappr.readthedocs.org/en/latest/setup/#autobranch'>docs</a>), ZAPPR will automatically
+                 create a branch in your repository for every opened issue.
               </p>,
-  commitmessage: <p>The commit message feature will check that commit messages in a pull request match at least one of
-                    some patterns you provide. For configuration options please consult <a href='https://zappr.readthedocs.org/en/latest/setup/#commitmessages'>our docs</a>.
+  commitmessage: <p>The commit message feature (<a href='https://zappr.readthedocs.org/en/latest/setup/#commitmessages'>docs</a>) will check that commit messages
+                    in a pull request match at least one of some patterns you provide.
                  </p>
 }
 
