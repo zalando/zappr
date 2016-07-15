70b74853dee156accfbb4a943a1bdc59caa00a6e
Nikolaus Piccolotto <prayerslayer@gmail.com>
2016-07-15 13:39:03 +0200

    Add docs about specification check (#356)
    * #353 add docs about specification check
    
    * #353 update according to new implementation

--- client/components/RepositoryCheck.jsx
+++ client/components/RepositoryCheck.jsx
@@ -4,17 +4,17 @@ import { Alert, Panel, Well } from 'react-bootstrap'
 import Toggle from './Toggle.jsx'
 
 const INFO_TEXT = {
-  approval: <p>The approval feature (<a href='https://zappr.readthedocs.org/en/latest/setup/#approvals'>docs</a>) blocks a pull request (if you
+  approval: <p>The approval feature (<a href='https://zappr.readthedocs.io/en/latest/setup/#approvals'>docs</a>) blocks a pull request (if you
                enabled <a href='https://github.com/blog/2051-protected-branches-and-required-status-checks'>protected branches</a>)
                until it has the required amount of approvals.
             </p>,
-  autobranch: <p>When enabling automatic branch creation (<a href='https://zappr.readthedocs.org/en/latest/setup/#autobranch'>docs</a>), ZAPPR will automatically
+  autobranch: <p>When enabling automatic branch creation (<a href='https://zappr.readthedocs.io/en/latest/setup/#autobranch'>docs</a>), ZAPPR will automatically
                  create a branch in your repository for every opened issue.
               </p>,
-  commitmessage: <p>The commit message feature (<a href='https://zappr.readthedocs.org/en/latest/setup/#commitmessages'>docs</a>) will check that commit messages
+  commitmessage: <p>The commit message feature (<a href='https://zappr.readthedocs.io/en/latest/setup/#commitmessages'>docs</a>) will check that commit messages
                     in a pull request match at least one of some patterns you provide.
                  </p>,
-  specification: <p>The specification check will verify that a pull request's title and body conform to the length and content requirements.</p>
+  specification: <p>The specification check (<a href='https://zappr.readthedocs.io/en/latest/setup/#specification'>docs</a>) will verify that a pull request's title and body conform to the length and content requirements.</p>
 }
 
 export default class RepositoryCheck extends Component {
