186958c1b8cbdb7950a997f44e38b6474ccb55de
Nikolaus Piccolotto <prayerslayer@gmail.com>
2016-07-22 14:40:08 +0200

    #315 possibility to ignore parts of user config (#383)

--- server/zapprfile/Configuration.js
+++ server/zapprfile/Configuration.js
@@ -1,6 +1,10 @@
 import yaml from 'js-yaml'
 import nconf from '../nconf'
 import mergeWith from 'lodash/mergeWith'
+import unset from 'lodash/unset'
+
+const DEFAULT_CONFIG = nconf.get('ZAPPR_DEFAULT_CONFIG') || {}
+const IGNORE_USER_CONFIG = nconf.get('IGNORE_USER_CONFIG') || []
 
 function mergeCustomizerFn(objValue, srcValue) {
   if (Array.isArray(objValue) && (Array.isArray(srcValue) || typeof srcValue === 'undefined')) {
@@ -9,18 +13,27 @@ function mergeCustomizerFn(objValue, srcValue) {
   }
 }
 
-const DEFAULT_CONFIG = nconf.get('ZAPPR_DEFAULT_CONFIG') || {}
+function getEffectiveConfiguration(userConfig, defaultConfig, ignorePaths) {
+  ignorePaths.forEach(path => unset(userConfig, path)) // unset mutates object ;_;
+  return mergeWith({}, defaultConfig, userConfig, mergeCustomizerFn)
+}
 
 export default class ZapprConfiguration {
-  constructor(fileContent, defaultConfig = DEFAULT_CONFIG) {
+  constructor(userConfig, defaultConfig = DEFAULT_CONFIG, ignorePaths = IGNORE_USER_CONFIG) {
     this.yamlParseError = null
     this.configuration = Object.assign({}, defaultConfig)
 
-    try {
-      const content = yaml.safeLoad(fileContent)
-      this.configuration = mergeWith({}, defaultConfig, content, mergeCustomizerFn)
-    } catch (e) {
-      this.yamlParseError = e.message
+    if (typeof userConfig === 'string') {
+      try {
+        const content = yaml.safeLoad(userConfig)
+        this.configuration = getEffectiveConfiguration(content, defaultConfig, ignorePaths)
+      } catch (e) {
+        this.yamlParseError = e.message
+      }
+    } else if (typeof userConfig === 'object' && !Array.isArray(userConfig)) {
+      this.configuration = getEffectiveConfiguration(userConfig, defaultConfig, ignorePaths)
+    } else {
+      throw new Error('ZapprConfiguration has to be called with a YAML String or a JSON object as first argument.')
     }
   }
 
