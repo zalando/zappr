a1444e67cc633b01ce25cbb9f4849163f92748e2
Nikolaus Piccolotto <prayerslayer@gmail.com>
2016-08-14 23:36:10 +0200

    #90 fix migrations again

--- common/ZapprModes.js
+++ common/ZapprModes.js
@@ -1,12 +1,4 @@
-// not using es6 modules because it will be read by umzug/sequelize
-
-const EXTENDED = 'extended'
-const MINIMAL = 'minimal'
-const COOKIE_NAME = 'zappr_mode'
-
-module.exports.EXTENDED = EXTENDED
-module.exports.MINIMAL = MINIMAL
-
-module.exports.COOKIE_NAME = COOKIE_NAME
-
-module.exports.MODES = [MINIMAL, EXTENDED]
+export const EXTENDED = 'extended'
+export const MINIMAL = 'minimal'
+export const COOKIE_NAME = 'zappr_mode'
+export const MODES = [EXTENDED, MINIMAL]
--- migrations/001-add-meta-info-to-check.js
+++ migrations/001-add-meta-info-to-check.js
@@ -1,45 +1,41 @@
-var Modes = require('../common/ZapprModes')
-
-module.exports.up = function up(query, Sequelize) {
+module.exports.up = function up(queryInterface, Sequelize) {
+  console.log(queryInterface)
+  const dialect = queryInterface.sequelize.options.dialect
   return Promise.all([
-                  query.addColumn({
-                      tableName: 'users',
-                      schema: 'zappr_data'
-                    },
-                    'zappr_mode',
-                    {
-                      type: Sequelize.ENUM(...Modes.MODES),
-                      allowNull: false,
-                      defaultValue: Modes.MINIMAL
-                    }),
-                  query.addColumn({
-                      tableName: 'checks',
-                      schema: 'zappr_data'
-                    },
-                    'created_by',
-                    {
-                      type: Sequelize.TEXT,
-                      allowNull: true
-                    })
-                ])
-                // this throws errors if the columns are already in place
-                // so we catch and log them
-                // sucks, but otherwise this would have to be a raw query supporting both dialects
-                .catch(console.log.bind(console))
+    /*
+     * Using raw query because User.sync() does:
+     *   - CREATE TYPE enum
+     *   - CREATE TABLE IF NOT EXISTS
+     * As migrations are run after sync(), the enum already exists.
+     * The migration also wants to create it, so it fails.
+     * Thus raw query referencing what should already be there.
+     */
+    queryInterface.sequelize.query(`
+            ALTER TABLE zappr_data.users
+            ADD COLUMN zappr_mode ${dialect === 'postgres' ? 'zappr_data.enum_users_zappr_mode' : 'TEXT'} NOT NULL DEFAULT 'minimal';`),
+    queryInterface.addColumn({
+        tableName: 'checks',
+        schema: 'zappr_data'
+      },
+      'created_by',
+      {
+        type: Sequelize.TEXT,
+        allowNull: true
+      })
+  ])
 }
 
 module.exports.down = function down(query) {
   return Promise.all([
-                  query.removeColumn({
-                      tableName: 'users',
-                      schema: 'zappr_data'
-                    },
-                    'zappr_mode'),
-                  query.removeColumn({
-                      tableName: 'checks',
-                      schema: 'zappr_data'
-                    },
-                    'created_by')
-                ])
-                .catch(console.log.bind(console))
+    query.removeColumn({
+        tableName: 'users',
+        schema: 'zappr_data'
+      },
+      'zappr_mode'),
+    query.removeColumn({
+        tableName: 'checks',
+        schema: 'zappr_data'
+      },
+      'created_by')
+  ])
 }
