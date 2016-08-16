4cd5119e5c8f2806838c559420ec53dd7c820412
Nikolaus Piccolotto <prayerslayer@gmail.com>
2016-08-16 20:24:54 +0200

    fix typo

--- test/server/util.test.js
+++ test/server/util.test.js
@@ -5,7 +5,7 @@ describe('server/util', () => {
   describe('jsonHash', () => {
     it('should hash an object', () => {
       const input = {approval: '😍'}
-      expect(hashJson(input)).to.equal('a1e5345e22fb40487c724595a9251a22aaf5ff820dee77e1a5222279115ed4d1')
+      expect(jsonHash(input)).to.equal('a1e5345e22fb40487c724595a9251a22aaf5ff820dee77e1a5222279115ed4d1')
     })
   })
 })
