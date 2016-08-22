99fa7d6564f4bb37ede0106f25b3a8b84a5d9723
Nikolaus Piccolotto <prayerslayer@gmail.com>
2016-08-22 23:01:31 +0200

    #90 change migrations as model.sync() will go away

--- migrations/001-add-meta-info-to-check.js
+++ migrations/001-add-meta-info-to-check.js
@@ -1,40 +1,41 @@
+const schema = 'zappr_data'
+const checksTable = 'checks'
+const usersTable = 'users'
+const accessLevelColumn = 'access_level'
+const createdByColumn = 'created_by'
+
 module.exports.up = function up(queryInterface, Sequelize) {
-  const dialect = queryInterface.sequelize.options.dialect
   return Promise.all([
-    /*
-     * Using raw query because User.sync() does:
-     *   - CREATE TYPE enum
-     *   - CREATE TABLE IF NOT EXISTS
-     * As migrations are run after sync(), the enum already exists.
-     * The migration also wants to create it, so it fails.
-     * Thus raw query referencing what should already be there.
-     */
-    queryInterface.sequelize.query(`
-            ALTER TABLE zappr_data.users
-            ADD COLUMN access_level ${dialect === 'postgres' ? 'zappr_data.enum_users_access_level' : 'TEXT'} NOT NULL DEFAULT 'minimal';`),
-    queryInterface.addColumn({
-        tableName: 'checks',
-        schema: 'zappr_data'
+    queryInterface.addColumn(
+      usersTable,
+      accessLevelColumn,
+      {
+        type: Sequelize.ENUM(['minimal', 'extended']),
+        allowNull: false,
+        defaultValue: 'minimal'
       },
-      'created_by',
+      {schema}),
+    queryInterface.addColumn(
+      checksTable,
+      createdByColumn,
       {
         type: Sequelize.TEXT,
         allowNull: true
-      })
+      },
+      {schema})
   ])
 }
 
 module.exports.down = function down(query) {
   return Promise.all([
-    query.removeColumn({
-        tableName: 'users',
-        schema: 'zappr_data'
-      },
-      'access_level'),
-    query.removeColumn({
-        tableName: 'checks',
-        schema: 'zappr_data'
-      },
-      'created_by')
+    query.removeColumn(
+      usersTable,
+      accessLevelColumn,
+      {schema}),
+
+    query.removeColumn(
+      checksTable,
+      createdByColumn,
+      {schema})
   ])
 }
