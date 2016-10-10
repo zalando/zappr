92133c0d49b33e9c4d63d52739782c8beebcac9f
Alper Kanat <me@alperkan.at>
2016-10-10 19:17:45 +0300

    added support for task lists (#440)

---
+++ server/checks/PullRequestTasks.js
@@ -0,0 +1,82 @@
+import Check, { getPayloadFn } from './Check'
+import { PULL_REQUEST } from '../model/GithubEvents'
+import { logger } from '../../common/debug'
+
+const CHECK_TYPE = 'pullrequesttasks'
+const CONTEXT = 'zappr/pr/tasks'
+const info = logger(CHECK_TYPE, 'info')
+const error = logger(CHECK_TYPE, 'error')
+const debug = logger(CHECK_TYPE)
+const createStatePayload = getPayloadFn(CONTEXT)
+
+function hasOpenTasks(pr) {
+  const regex = /(-|\*) \[(\s|x)\]/g;
+  let count = 0;
+  let repoName = pr.head.repo.full_name;
+  let prNo = pr.number;
+  let match;
+
+  while ((match = regex.exec(pr.body)) !== null) {
+    // This is necessary to avoid infinite loops with zero-width matches
+    if (match.index === regex.lastIndex) {
+      regex.lastIndex++;
+    }
+
+    let task = match[0];
+    let marker = match[2];
+
+    debug(`${repoName}#${prNo}: Processing task: ${task}`);
+
+    if (marker === " ") {
+      count++;
+    }
+  }
+
+  info(`${repoName}#${prNo}: PR has ${count} open tasks.`);
+
+  return count > 0;
+}
+
+export default class PullRequestTasks extends Check {
+  static TYPE = CHECK_TYPE;
+  static CONTEXT = CONTEXT;
+  static HOOK_EVENTS = [PULL_REQUEST];
+  static NAME = 'Pull request tasks check'
+
+  constructor(github) {
+    super()
+    this.github = github;
+  }
+
+  async execute(config, hookPayload, token) {
+    const {action, repository, number, pull_request} = hookPayload
+    const repoOwner = repository.owner.login
+    const repoName = repository.name
+    const fullName = repository.full_name
+    let status;
+
+    try {
+      // TODO: configuration settings
+
+      if (pull_request.state === 'open' && ['opened', 'edited', 'reopened'].indexOf(action) !== -1) {
+        let msg;
+
+        if (hasOpenTasks(pull_request)) {
+          msg = `${fullName}#${number}: Failed the PR due to open tasks.`;
+          info(msg);
+          status = createStatePayload(msg, 'failure');
+        } else {
+          msg = `${fullName}#${number}: PR has no open tasks.`;
+          info(msg);
+          status = createStatePayload(msg);
+        }
+
+        await this.github.setCommitStatus(repoOwner, repoName, pull_request.head.sha, status, token);
+      }
+    } catch (e) {
+      error(`${fullName}#${number}: Could not execute Pull Request Tasks check`, e)
+      status = createStatePayload(`Error: ${e.message}`, 'error')
+      await this.github.setCommitStatus(repoOwner, repoName, pull_request.head.sha, status, token);
+    }
+  }
+}
