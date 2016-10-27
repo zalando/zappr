450c8624e6698a0ef517501faf49523d23a1aa11
Nikolaus Piccolotto <prayerslayer@gmail.com>
2016-10-27 13:35:12 +0200

    Check existing pull requests (#464)
    * #461 update node to Dockerfile-version
    
    * #461 consistent api for check.execute
    
    * #461 save wip
    
    * #461 shuffling things around
    
    * #461 mostly formatting
    
    * #461 unit tests
    
    * #461 fix tests
    
    * #461 fix tests
    
    * #461 enable logs in test
    
    * #461 bugfixes
    
    * #461 revert unrelated stuff
    
    * #461 fix test
    
    * #461 revert approval
    
    * #461 revert the rest
    
    * #461 revert check class
    
    * #461 more reverts
    
    * #461 and some more
    
    * #461 fix missing repo id
    
    * #461 nonblocking operations when in production
    
    * #461 support for more than 30 (?) pull requests
    
    * #461 naming

--- test/server/api.test.js
+++ test/server/api.test.js
@@ -63,6 +63,8 @@ describe('API', () => {
       fixtures.invalidZappr = require('../fixtures/github.zapprfile.invalid.json')
       fixtures.noZappr = require('../fixtures/github.zapprfile.notfound.json')
 
+      fixtures.pullRequests = require('../fixtures/github.pull_requests.json')
+      fixtures.pullRequestComments = require('../fixtures/github.pull_request.comments.json')
       // Configure mountebank
       const mb = await mountebank.start()
       // @formatter:off
@@ -273,6 +275,50 @@ describe('API', () => {
                 .predicate()
                   .setPath(`/repos/${fixtures.repoOwner}/${fixtures.repoName}/branches/master/protection/required_status_checks`)
                   .setMethod('PATCH')
+                .add()
+              .add()
+              .stub()
+                .response()
+                  .setStatusCode(200)
+                  .setHeader('Content-Type', 'application/json')
+                  .setBody(fixtures.pullRequests)
+                .add()
+                .predicate()
+                  .setPath(`/repos/${fixtures.repoOwner}/${fixtures.repoName}/pulls`)
+                  .setMethod('GET')
+                .add()
+              .add()
+              .stub()
+                .response()
+                  .setStatusCode(200)
+                  .setHeader('Content-Type', 'application/json')
+                  .setBody(fixtures.pullRequests)
+                .add()
+                .predicate()
+                  .setPath(`/repos/${fixtures.repo2FullName}/pulls`)
+                  .setMethod('GET')
+                .add()
+              .add()
+              .stub()
+                .response()
+                  .setStatusCode(200)
+                  .setHeader('Content-Type', 'application/json')
+                  .setBody(fixtures.pullRequestComments)
+                .add()
+                .predicate()
+                  .setPath(`/repos/${fixtures.repoOwner}/${fixtures.repoName}/issues/${fixtures.pullRequests[0].number}/comments`)
+                  .setMethod('GET')
+                .add()
+              .add()
+              .stub()
+                .response()
+                  .setStatusCode(200)
+                  .setHeader('Content-Type', 'application/json')
+                  .setBody(fixtures.pullRequestComments)
+                .add()
+                .predicate()
+                  .setPath(`/repos/${fixtures.repo2FullName}/issues/${fixtures.pullRequests[0].number}/comments`)
+                  .setMethod('GET')
                 .add()
               .add()
               .create()
@@ -538,9 +584,11 @@ describe('API', () => {
          * zapprfiles
          * get, update branch protection
          * get hooks, add hook
+         * run approval check on pull requests
+         * remove approval status on pull requests
          * get hooks, remove hook
          */
-        expect(calls.length).to.equal(11)
+        expect(calls.length).to.equal(16)
         expect(call(calls[0])).to.equal('GET /user/repos')
         expect(call(calls[1])).to.match(/^GET \/repos\/.+?\/contents\/\.zappr\.ya?ml$/)
         expect(call(calls[2])).to.match(/^GET \/repos\/.+?\/contents\/\.zappr\.ya?ml$/)
@@ -550,12 +598,17 @@ describe('API', () => {
           `PATCH /repos/${fullName}/hooks/123`,
           `GET /repos/${fullName}/branches/master`,
           `PUT /repos/${fullName}/branches/master/protection`,
+          `GET /repos/${fullName}/pulls`,
+          `GET /repos/${fullName}/issues/${fixtures.pullRequests[0].number}/comments`,
+          `POST /repos/${fullName}/statuses/${fixtures.pullRequests[0].head.sha}`,
+          `GET /repos/${fullName}/pulls`,
+          `POST /repos/${fullName}/statuses/${fixtures.pullRequests[0].head.sha}`,
           `GET /repos/${fullName}/branches/master/protection/required_status_checks`,
           `PATCH /repos/${fullName}/branches/master/protection/required_status_checks`,
           `GET /repos/${fullName}/hooks`,
           `DELETE /repos/${fullName}/hooks/123`
         ])
-        const updateSettings = JSON.parse(rest[5].body)
+        const updateSettings = JSON.parse(rest[10].body)
         expect(updateSettings).to.deep.equal({
           include_admins: true,
           contexts: ['travis-ci']
@@ -591,8 +644,9 @@ describe('API', () => {
          * 7) create PR
          * 8,9) get, update web hooks
          * 10,11) get, update branch protection
+         * 12-14) fetch pull requests and update status on those
          */
-        expect(calls.length).to.equal(11)
+        expect(calls.length).to.equal(14)
         expect(call(calls[0])).to.equal('GET /user/repos')
         // 2+3 are much async and interchangeable
         expect(call(calls[1])).to.match(/^GET \/repos\/.+?\/contents\/\.zappr\.ya?ml$/)
@@ -607,6 +661,9 @@ describe('API', () => {
           `PATCH /repos/${fixtures.repo2FullName}/hooks/123`,
           `GET /repos/${fixtures.repo2FullName}/branches/master`,
           `PUT /repos/${fixtures.repo2FullName}/branches/master/protection`,
+          `GET /repos/${fixtures.repo2FullName}/pulls`,
+          `GET /repos/${fixtures.repo2FullName}/issues/${fixtures.pullRequests[0].number}/comments`,
+          `POST /repos/${fixtures.repo2FullName}/statuses/${fixtures.pullRequests[0].head.sha}`,
         ])
         const createBranchBody = JSON.parse(rest[1].body)
         const createZapprBody = JSON.parse(rest[2].body)
@@ -656,8 +713,9 @@ describe('API', () => {
          * 2.+3. get zapprfile
          * 4.+5. get hooks, add hook
          * 6.+7. check if branch is protected, update protection
+         * 8.-10. fetch pull requests and update status on those
          */
-        expect(calls.length).to.equal(7)
+        expect(calls.length).to.equal(10)
         expect(call(calls[0])).to.equal('GET /user/repos')
         // 2+3 are much async and interchangeable
         expect(call(calls[1])).to.match(/^GET \/repos\/.+?\/contents\/\.zappr\.ya?ml$/)
@@ -668,6 +726,9 @@ describe('API', () => {
           `PATCH /repos/${fullName}/hooks/123`,
           `GET /repos/${fullName}/branches/master`,
           `PUT /repos/${fullName}/branches/master/protection`,
+          `GET /repos/${fullName}/pulls`,
+          `GET /repos/${fullName}/issues/${fixtures.pullRequests[0].number}/comments`,
+          `POST /repos/${fullName}/statuses/${fixtures.pullRequests[0].head.sha}`,
         ])
         // branch protection call should have approval context
         const protectionSettings = JSON.parse(rest[3].body)
