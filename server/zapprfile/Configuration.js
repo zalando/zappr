9ece2bbd3cc6d708bbbedf62215f3d5b8ca6107e
Nikolaus Piccolotto <prayerslayer@gmail.com>
2017-03-03 13:33:36 +0100

    allow some orgs to do whatever

--- server/zapprfile/Configuration.js
+++ server/zapprfile/Configuration.js
@@ -5,6 +5,7 @@ import unset from 'lodash/unset'
 
 const DEFAULT_CONFIG = nconf.get('ZAPPR_DEFAULT_CONFIG') || {}
 const IGNORE_USER_CONFIG = nconf.get('IGNORE_USER_CONFIG') || []
+const IGNORE_NOT_IN_ORGS = nconf.get('IGNORE_USER_CONFIG_NOT_IN_ORGS') || []
 
 function mergeCustomizerFn(objValue, srcValue) {
   if (Array.isArray(objValue) && (Array.isArray(srcValue) || typeof srcValue === 'undefined')) {
@@ -13,25 +14,28 @@ function mergeCustomizerFn(objValue, srcValue) {
   }
 }
 
-function getEffectiveConfiguration(userConfig, defaultConfig, ignorePaths) {
-  ignorePaths.forEach(path => unset(userConfig, path)) // unset mutates object ;_;
+function getEffectiveConfiguration(userConfig, repository, defaultConfig, ignorePaths, ignoreNotInOrgs) {
+  const shouldPass = ignoreNotInOrgs.length > 0 && ignoreNotInOrgs.some(org => org === repository.json.organization.login)
+  if (!shouldPass) {
+    ignorePaths.forEach(path => unset(userConfig, path)) // unset mutates object ;_;
+  }
   return mergeWith({}, defaultConfig, userConfig, mergeCustomizerFn)
 }
 
 export default class ZapprConfiguration {
-  constructor(userConfig, defaultConfig = DEFAULT_CONFIG, ignorePaths = IGNORE_USER_CONFIG) {
+  constructor(userConfig, repository = {}, defaultConfig = DEFAULT_CONFIG, ignorePaths = IGNORE_USER_CONFIG, ignoreNotInOrgs = IGNORE_NOT_IN_ORGS) {
     this.yamlParseError = null
     this.configuration = Object.assign({}, defaultConfig)
 
     if (typeof userConfig === 'string') {
       try {
         const content = yaml.safeLoad(userConfig)
-        this.configuration = getEffectiveConfiguration(content, defaultConfig, ignorePaths)
+        this.configuration = getEffectiveConfiguration(content, repository, defaultConfig, ignorePaths, ignoreNotInOrgs)
       } catch (e) {
         this.yamlParseError = e.message
       }
     } else if (typeof userConfig === 'object' && !Array.isArray(userConfig)) {
-      this.configuration = getEffectiveConfiguration(userConfig, defaultConfig, ignorePaths)
+      this.configuration = getEffectiveConfiguration(userConfig, repository, defaultConfig, ignorePaths, ignoreNotInOrgs)
     } else {
       throw new Error('ZapprConfiguration has to be called with a YAML String or a JSON object as first argument.')
     }
