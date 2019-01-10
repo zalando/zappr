f01b0a1fc7911c3d1f67947a6e1a835f20f8f06d
lotharschulz <lothar.schulz@zalando.de>
2019-01-10 11:11:56 +0100

    run zappr local shell script

--- run-zappr-local.sh
+++ run-zappr-local.sh
@@ -1,8 +1,14 @@
+#!/bin/bash
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+pidof -o $$ -x "$0" >/dev/null 2>&1 && exit 1
+
 echo "DB_HOST: ${DB_HOST}"
 echo "DM_IP: ${DM_IP}"
-echo
 eval "$(docker-machine env default)"
-echo $(docker-machine env default)
+echo "docker-machine env default: "; echo $(docker-machine env default);
 GCI=$GITHUB_CLIENT_ID
 short="${GCI:0:5}" ; echo "GITHUB_CLIENT_ID: ${short}....";
 GCS=$GITHUB_CLIENT_SECRET
