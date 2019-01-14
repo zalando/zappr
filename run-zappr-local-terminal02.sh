ff877bdfceaff193dffa585665ff4427c570d100
lotharschulz <lothar.schulz@zalando.de>
2019-01-14 15:11:19 +0100

    DB_HOST and DM_IP added to run-zappr-local-terminal02.sh

--- run-zappr-local-terminal02.sh
+++ run-zappr-local-terminal02.sh
@@ -3,6 +3,11 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+eval "$(docker-machine env default)"
+export DM_IP="$(docker-machine ip)"
+export DB_HOST="$(docker-machine ip)"
+echo "DB_HOST: ${DB_HOST} - DM_IP: ${DM_IP}"
+
 ./init_db.sh || true
 
 echo "issuing npm install, npm run build & npm run all with env variables:"
