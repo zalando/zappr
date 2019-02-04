89257e573273e01ad019dfdacd36cf2a13959247
lotharschulz <lothar.schulz@zalando.de>
2019-02-04 22:18:57 +0100

    test description adapted to disabled branch protection

--- test/server/github.test.js
+++ test/server/github.test.js
@@ -168,7 +168,7 @@ describe('The Github service', () => {
       }
     })
 
-    it('removeRequiredStatusCheck returns 403 when branch protection exists', async(done) => {
+    it('removeRequiredStatusCheck returns 403 when changing branch protection is disabled', async(done) => {
         //GIVEN
         const errorConfig = {
           statusCode: 403,
