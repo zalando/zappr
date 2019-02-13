20c9ab1141ac5eb285aecdb7478f52b88850382f
Rechi <Rechi@users.noreply.github.com>
2019-02-13 16:35:58 +0100

    fix: add merge commits check to enum_checks_type after c12f8f8

---
+++ migrations/008-add-prmergecommit-enum-value.js
@@ -0,0 +1,8 @@
+module.exports.up = function up(queryInterface) {
+  return queryInterface.sequelize.query(`ALTER TYPE zappr_data.enum_checks_type ADD VALUE IF NOT EXISTS 'pullrequestmergecommit';`)
+}
+
+module.exports.down = function down() {
+  // there is no good downgrade
+  return Promise.resolve()
+}
