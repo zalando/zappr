7117021e17d15f5d413a7c49e5b038ae6f197711
Fabian Wollert <fabian@zalando.de>
2019-02-22 14:12:14 +0100

    debug

--- server/checks/Check.js
+++ server/checks/Check.js
@@ -1,6 +1,6 @@
 export function getPayloadFn(context) {
   return function createStatePayload(description, state = 'success') {
-    console.log('createStatePayload', state, context, description);
+    console.log('Check.js createStatePayload', state, context, description);
     return {
       state,
       context,
