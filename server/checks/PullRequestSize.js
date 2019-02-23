59ff01a6dcadd315b7bf2186e71459bf38aacd20
Fabian Wollert <fabian@zalando.de>
2019-02-23 16:16:25 +0100

    basic implemantion

---
+++ server/checks/PullRequestSize.js
@@ -0,0 +1,75 @@
+import Check, { getPayloadFn } from './Check'
+import { PULL_REQUEST } from '../model/GithubEvents'
+import { logger } from '../../common/debug'
+
+const CHECK_TYPE = 'pullrequestsize'
+const CONTEXT = 'zappr/pr/size'
+const info = logger(CHECK_TYPE, 'info')
+const error = logger(CHECK_TYPE, 'error')
+const debug = logger(CHECK_TYPE)
+const createStatePayload = getPayloadFn(CONTEXT)
+
+
+export default class PullRequestTasks extends Check {
+  static TYPE = CHECK_TYPE;
+  static CONTEXT = CONTEXT;
+  static HOOK_EVENTS = [PULL_REQUEST];
+  static NAME = 'Pull request size check'
+
+  constructor(github) {
+    super()
+    this.github = github;
+  }
+
+  async checkPRSizeAndSetStatus({pull_request, repository, token, config}) {
+    const {fullName, name, owner} = repository
+    const {number, additions, deletions, changed_files} = pull_request
+    const max_additions = getIn(config, ['pull-request', 'size', 'max', 'additions'], [])
+    const max_deletions = getIn(config, ['pull-request', 'size', 'max', 'deletions'], [])
+    const max_changed_files = getIn(config, ['pull-request', 'size', 'max', 'changed_files'], [])
+
+    const checkEnabled = max_additions.length > 0 ? max_deletions.length > 0 ? max_changed_files.length > 0 : false : false
+    let msg, status;
+
+    if (checkEnabled) {
+      if (additions > max_additions) {
+        info(`${fullName}#${number}: Failed the PR due to more additions than allowed.`);
+        status = createStatePayload(`PR adds too much lines.`, 'failure');
+      } else if (deletions > max_deletions) {
+        info(`${fullName}#${number}: Failed the PR due to more deletions than allowed.`);
+        status = createStatePayload(`PR deletes too much lines.`, 'failure');
+      } else if (changed_files > max_changed_files) {
+        info(`${fullName}#${number}: Failed the PR due to more files changed than allowed.`);
+        status = createStatePayload(`PR changes too much files.`, 'failure');
+      } else {
+        msg = `PR Size restrictions are met.`;
+        info(`${fullName}#${number}: ${msg}`);
+        status = createStatePayload(msg);
+      }
+    } else {
+      info(`${fullName}#${number}: No PR size settings set.`);
+      status = createStatePayload(`No PR size settings set.`);
+    }
+
+    await this.github.setCommitStatus(owner.login, name, pull_request.head.sha, status, token);
+    info(`${fullName}#${number}: Set status to ${status.state}.`)
+  }
+
+  async execute(config, hookPayload, token) {
+    const {action, repository, number, pull_request} = hookPayload
+    const repoOwner = repository.owner.login
+    const repoName = repository.name
+    const fullName = repository.full_name
+    let status;
+
+    try {
+      if (pull_request.state === 'open' && ['opened', 'edited', 'reopened', 'synchronize'].indexOf(action) !== -1) {
+        await this.checkPRSizeAndSetStatus({pull_request, repository, token, config})
+      }
+    } catch (e) {
+      error(`${fullName}#${number}: Could not execute ${NAME}`, e)
+      status = createStatePayload(`Error: ${e.message}`, 'error')
+      await this.github.setCommitStatus(repoOwner, repoName, pull_request.head.sha, status, token);
+    }
+  }
+}
--- server/checks/index.js
+++ server/checks/index.js
@@ -6,6 +6,7 @@ import PullRequestMilestone from './PullRequestMilestone'
 import PullRequestLabels from './PullRequestLabels'
 import PullRequestMergeCommit from './PullRequestMergeCommit'
 import PullRequestTasks from './PullRequestTasks'
+import PullRequestSize from './PullRequestSize'
 
 const CHECKS = {
   [Approval.TYPE]: Approval,
@@ -16,6 +17,7 @@ const CHECKS = {
   [PullRequestLabels.TYPE]: PullRequestLabels,
   [PullRequestMergeCommit.TYPE]: PullRequestMergeCommit,
   [PullRequestTasks.TYPE]: PullRequestTasks
+  [PullRequestSize.TYPE]: PullRequestSize
 }
 
 export const CHECK_NAMES = {
@@ -27,6 +29,7 @@ export const CHECK_NAMES = {
   [PullRequestLabels.TYPE]: PullRequestLabels.NAME,
   [PullRequestMergeCommit.TYPE]: PullRequestMergeCommit.NAME,
   [PullRequestTasks.TYPE]: PullRequestTasks.NAME
+  [PullRequestSize.TYPE]: PullRequestSize.NAME
 }
 
 export const CHECK_TYPES = Object.keys(CHECKS)
@@ -35,4 +38,4 @@ export function getCheckByType(type) {
   return CHECKS[type]
 }
 
-export { Approval, Autobranch, CommitMessage, Specification, PullRequestMilestone, PullRequestLabels, PullRequestMergeCommit, PullRequestTasks }
+export { Approval, Autobranch, CommitMessage, Specification, PullRequestMilestone, PullRequestLabels, PullRequestMergeCommit, PullRequestTasks, PullRequestSize }
